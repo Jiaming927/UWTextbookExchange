058b06b9e968b0770f80ea01c77fc1bbc8fb382a
computist <yingkaiwangd@gmail.com>
2014-05-25 06:36:38 -0400

    query for course book

--- UWTextbookExchange/app/controllers/get_book_controller.rb
+++ UWTextbookExchange/app/controllers/get_book_controller.rb
@@ -3,10 +3,20 @@ class GetBookController < ApplicationController
   end
 
   def read
-	@course = CourseToBook.new
+	#@course = CourseToBook.new
 	if params[:post]
-	    redirect_to(:action => 'update')
-	end	
+	    	 flash[:notice] = params[:course]["department"]
+		redirect_to(:action => 'update')
+	end
+	
+	courseName = params[:course]["department"]
+	courseName <<  params[:course]["class"]
+	
+	@courseInfo = CourseToBook.where(:course => courseName).first
+	if !@courseInfo
+	   flash[:notice] = "no book found, check  your input"
+	  redirect_to(:controller => 'init', :action =>'index')
+	end
   end
 
   def update
