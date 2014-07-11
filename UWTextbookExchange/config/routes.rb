d62e02f2247aa01f66007973f3ce5f3ddaf7386a
computist <yingkaiwangd@gmail.com>
2014-07-11 00:12:51 -0400

    optimize route

--- UWTextbookExchange/config/routes.rb
+++ UWTextbookExchange/config/routes.rb
@@ -1,6 +1,10 @@
-Rails.application.routes.draw do
-  devise_for :users
-
+Rails.application.routes.draw do  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
+  devise_scope :user do
+    get 'resend', :to => 'devise/confirmations#new'
+    get 'passreset', :to => 'devise/passwords#new'  
+  end
+  
   root :to => "init#index"
   match ':controller(/:action(/:id))', :via => [:get, :post]
   match "*path", to: redirect("/"), via: :all
