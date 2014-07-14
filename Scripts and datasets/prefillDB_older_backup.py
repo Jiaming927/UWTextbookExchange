a3ea29ff30a5aa2c7290c5c7bc0ce39c920d0ca4
Jiaming927 <masterjm@cs.washington.edu>
2014-07-14 20:56:30 +0800

    prefillDB should be done

--- Scripts and datasets/prefillDB_older_backup.py
+++ Scripts and datasets/prefillDB_older_backup.py
@@ -11,8 +11,6 @@ lines = file.readlines()
 course_name = ""
 book = ""
 queryContent = ""
-queryStart = ""
-queryBooks = ""
 finalQuery = ""
 i = 0
 
@@ -20,21 +18,20 @@ while i < len(lines):
 	if lines[i].startswith("*"):
 		course_name = lines[i][1:].strip()
 		i += 1
-		queryStart = "\"" + course_name + "\"" +
+		queryContent = "\"" + course_name + "\""
 	else:
 		print course_name + "...START \n"
 		index = 1
 		bookCount = "book1"
 		while i < len(lines) and not lines[i].startswith("*"):
-			queryBooks += ", \"" + lines[i].strip() + "*" + lines[i + 1].strip() + "*" + lines[i + 2].strip() + "\""
+			queryContent += ", \"" + lines[i].strip() + "*" + lines[i + 1].strip() + "*" + lines[i + 2].strip() + "\""
 			if not index == 1:
 				bookCount += ",book" + str(index)
 			index += 1
 			i += 3
 		
 		print course_name + "...DONE \n"
-		queryContent = queryStart + ",\"" + str(index) + "\"" + queryBooks
-		finalQuery = "INSERT INTO courses " + "(course_name, number, " + bookCount + ") VALUES(" + queryContent + ")"
+		finalQuery = "INSERT INTO courses " + "(course_name, " + bookCount + ") VALUES(" + queryContent + ")"
 		db.execute(finalQuery)
 
 db.commit()
@@ -49,7 +46,7 @@ db.close()
 
 # books = "book"
 
-# INSERT INTO courses (course_name, number, book1) VALUES("CSE 142", str(number), "BUILDING JAVA PROGRAM");
+# INSERT INTO courses (course_name, book1) VALUES("CSE 142", "BUILDING JAVA PROGRAM");
 
 # query = "INSERT INTO books (ID,NAME,AGE,ADDRESS,SALARY) \ VALUES (1, 'Paul', 32, 'California', 20000.00 )"
 
