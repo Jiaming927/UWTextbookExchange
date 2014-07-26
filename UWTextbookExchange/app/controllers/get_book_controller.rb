65a84a0451c0a441b539b837ead0f9e36043fb2b
computist <yingkaiwangd@gmail.com>
2014-07-26 15:28:04 -0400

    rewrite get book controller and pages

--- UWTextbookExchange/app/controllers/get_book_controller.rb
+++ UWTextbookExchange/app/controllers/get_book_controller.rb
@@ -4,30 +4,32 @@ class GetBookController < ApplicationController
   def find
 	if params["coursename"]
 		session[:coursename] = params["coursename"]
-		@courseName = params["coursename"].strip.upcase
-		@courseInfo = Course.where(:course_name => @courseName).first
-		if !@courseInfo
+		courseName = params["coursename"].strip.upcase
+		@course = Course.where(:course_name => courseName).first
+		if !@course
 		  flash[:notice] = "This course is not held in this quarter, or misspell anything?"
 		  return redirect_to(root_url)
 		end
+		createInfo(@course)
 		messagehelper
 	elsif params[:course]
 		session[:last_dep] = params[:course]["department"].strip
 		session[:last_class] = params[:course]["class"].strip
-		@courseName = params[:course]["department"].strip.upcase
-		@courseName <<  params[:course]["class"].strip
-		session[:coursename] = @courseName
+		courseName = params[:course]["department"].strip.upcase
+		courseName <<  params[:course]["class"].strip
+		session[:coursename] = courseName
 
 		if params[:post]
-		    	 flash[:notice] = params[:course]["department"]
+		    	flash[:notice] = params[:course]["department"]
 			return redirect_to('/post')
 		else	
-			@courseInfo = Course.where(:course_name => @courseName).first
-			if !@courseInfo
+			@course = Course.where(:course_name => courseName).first
+			if !@course
 			  flash[:notice] = "This course is not held in this quarter, or misspell anything?"
 			  return redirect_to(root_url)
 			end
 		end
+		createInfo(@course)
 		messagehelper
 	else
 		redirect_to(root_url)
@@ -36,106 +38,48 @@ class GetBookController < ApplicationController
 
   def show
 	if params[:book]
-		@showbookname = params[:book].strip
-		@bookinfo = Book.where(:book_title => @showbookname).first
+		@book = Book.where(:book_title => params[:book].strip)
+		@bookinfo = Bookinfo.where(:book_title => params[:book].strip).first
 		messagehelper
-		if params[:course]
-			@showcoursename = params[:course].strip
-		end
 	else
 		redirect_to(root_url)
 	end
   end
 
   def postto
-	if params[:course] && params[:book]
-		course = params[:course].strip
+	if params[:book]
 		bookname = params[:book].strip
+		#if book in bookinfo
+		binfo = Bookinfo.where(:book_title => bookname).first
+		if binfo
+			coursename = binfo.course_name
+			#increment bookinfo
+			binfo.number = binfo.number + 1
+			if binfo.save
+				#add to book
+				Book.create(:book_title => bookname, :username => current_user.username)
 
-		#update userbook
-		@userbook_edit = Userbooks.where(:email => current_user.email).first
-		exist = nil
-		if !@userbook_edit
-			#userbook record not exist
-			@userbook_edit = Userbooks.new
-			@userbook_edit.email = current_user.email
-			@userbook_edit.username = current_user.username
-		else
-			#record exist
-			# test if the book already exist in the record
-			for i in 1..@userbook_edit.own			
-				owncolumn = "ownedbook" + i.to_s
-				if @userbook_edit.read_attribute(owncolumn.to_sym) == bookname.strip
-					exist = 1;
-				end
+				#add to owned
+				Ownedbook.create(:book_title => bookname, :username => current_user.username)
+				flash[:postmessage] = bookname + " is posted successfully"
+				return redirect_to('/post')
 			end
-		end
-		
-		if !exist
-			if @userbook_edit.own != 15
-				@userbook_edit.own = @userbook_edit.own + 1
-				@userbookcolumn = "ownedbook" + @userbook_edit.own.to_s
-				@userbook_edit.assign_attributes({@userbookcolumn.to_sym => bookname})
-				if @userbook_edit.save
-					# update books
-					@book_edit = Book.where(:book_title => bookname).first
-					if !@book_edit
-						#book not exist
-						@book_edit = Book.new
-						@book_edit.book_title = bookname
-						@book_edit.course_name = course
-					end
-
-					if @book_edit.number == 500
-						#overflow
-						@book_edit.number = @book_edit.number - 1
-						for i in 1..499
-							prevcolumn = "user" + i.to_s
-							nextcolumn = "user" + (i+1).to_s
-							nextvalue = @book_edit.read_attribute(nextcolumn.to_sym)
-							@book_edit.assign_attributes({prevcolumn.to_sym => nextvalue})
-						end
-						@book_edit.assign_attributes({:user500 => nil})
-					end
-					@book_edit.number = @book_edit.number + 1
-					@usercolumn = "user" + @book_edit.number.to_s
-					@book_edit.assign_attributes({@usercolumn.to_sym => current_user.username})
-					if @book_edit.save
-						#success
-						flash[:postmessage] = bookname + " is posted successfully."
-						return 	redirect_to('/post')
-					end
-
-					# failed to redirect
-					# retrieve database
-					@userbook_edit.own = @userbook_edit.own - 1
-					@userbook_edit.assign_attributes({userbookcolumn.to_sym => nil})
-					if !@userbook_edit.save
-						# retrieve error
-					end
-				end
-				flash[:postmessage] = "Error occurs. We are working on it right now. Sorry for the inconvenience."
-			else
-
-				# overflow
-				flash[:editbookmessage] = "You cannot post more than 15 books. Please unlist some of them"
-				return 	redirect_to('/personal')	
-			end
-		else
-			flash[:postmessage] = "You have already had that book."
 		end
 	end
-	redirect_to('/post')
+	redirect_to(root_url)
   end
 
   def post
 	if session[:coursename]
-		@courseInfo = Course.where(:course_name => session[:coursename]).first
-		if !@courseInfo
+		@course = Course.where(:course_name => session[:coursename]).first
+		if !@course
 		  flash[:notice] = "This course is not held in this quarter, or misspell anything?"
 		  return redirect_to(root_url)
 		end
+		createInfo(@course)
 		messagehelper
+	else
+		redirect_to(root_url)
 	end
   end
 
@@ -143,6 +87,17 @@ private
     def messagehelper
 	if user_signed_in?
 		@msgc = Msgcount.where(:username => current_user.username).first
+	end
+    end
+
+    def createInfo(course)
+	course.attributes.each do |attr_name, attr_value|
+		if attr_name != "id" && attr_name != "course_name" && attr_name != "number" && attr_value != nil %>
+			binfo = Bookinfo.where(:book_title => attr_value).first
+			if !binfo
+				Bookinfo.create(:book_title => attr_value, :course_name => course.course_name)
+			end
+		end
 	end
     end
 end
