695912ae97d96ad61ba6ac89a36210b811c645f0
computist <yingkaiwangd@gmail.com>
2014-08-22 14:37:03 +0000

    optimize script, prefiled bookinfo

--- Scripts and datasets/prefillDB.py
+++ Scripts and datasets/prefillDB.py
@@ -1,6 +1,7 @@
 import sqlite3 
 
-db = sqlite3.connect("../UWTextbookExchange/db/development.sqlite3")
+db = sqlite3.connect("../UWTextbookExchange/db/production.sqlite3")
+cur = db.cursor()
 print "Connection established"
 file = open("*TextBookFile.txt")
 print "File found\n"
@@ -14,6 +15,10 @@ queryContent = ""
 queryStart = ""
 queryBooks = ""
 finalQuery = ""
+new = ""
+newcount = 0
+exist = ""
+existcount = 0
 i = 0
 
 while i < len(lines):
@@ -23,35 +28,42 @@ while i < len(lines):
 		i += 1
 		queryStart = "\"" + course_name + "\""
 	else:
-		print course_name + "...START \n"
 		index = 1
 		bookCount = "book1"
 		while i < len(lines) and not lines[i].startswith("*"):
-			queryBooks += ", \"" + lines[i].strip() + "*" + lines[i + 1].strip() + "*" + lines[i + 2].strip() + "\""
+			book_title = lines[i][6:].strip()
+			author = lines[i+1][8:].strip()
+			price = lines[i+2][7:].strip()
+			cur.execute("SELECT id, course_name FROM bookinfos WHERE book_title = ?", (book_title,))
+			result = cur.fetchone()			
+			if result is None:
+				bookinfo_query = "INSERT INTO bookinfos (book_title, author, price, course_name) VALUES(" + "\"" + book_title + "\"" + ", \"" + author + "\""+ ", \"" + price + "\"" + ", \"" + course_name + "\"" + ")"
+				db.execute(bookinfo_query)
+			else:
+				if course_name not in result[1]:
+					add_course = ','.join(sorted((result[1] + "," + course_name).split(',')))
+					cur.execute("UPDATE bookinfos SET course_name=? WHERE id=?", (add_course, result[0]))		
+			queryBooks += ", \"" + book_title + "\""
 			if not index == 1:
 				bookCount += ",book" + str(index)
 			index += 1
-			i += 3
-		
-		print course_name + "...DONE \n"
-		queryContent = queryStart + ",\"" + str(index) + "\"" + queryBooks
-		finalQuery = "INSERT INTO courses " + "(course_name, number, " + bookCount + ") VALUES(" + queryContent + ")"
-		db.execute(finalQuery)
-	queryBooks=""
+			i += 3		
+		queryContent = queryStart + ",\"" + str(index-1) + "\"" + queryBooks
+		queryBooks=""
+		cur.execute("SELECT course_name FROM courses WHERE course_name = ?", (course_name,))
+		result = cur.fetchone()			
+		if result is None:
+			finalQuery = "INSERT INTO courses " + "(course_name, number, " + bookCount + ") VALUES(" + queryContent + ")"
+			db.execute(finalQuery)
+			new += course_name + ", "
+			newcount+=1
+		else:
+			exist += course_name + ", "
+			existcount+=1
 db.commit()
 db.close()
-#"INSERT INTO courses("
-
-# books = "book1"
-
-# #strip everything before this
-# for i in len(textbooks) - 1:
-# 	books += "book" + str(i)
-
-# books = "book"
-
-# INSERT INTO courses (course_name, number, book1) VALUES("CSE 142", str(number), "BUILDING JAVA PROGRAM");
-
-# query = "INSERT INTO books (ID,NAME,AGE,ADDRESS,SALARY) \ VALUES (1, 'Paul', 32, 'California', 20000.00 )"
-
-# print query
+print "Add " + str(newcount) + " courses \n"
+print new
+print "============================================================\n"
+print str(existcount) + " courses already exist \n"
+print exist
